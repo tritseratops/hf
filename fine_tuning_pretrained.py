84ffc1a02fdbe6e3c69815489f7a8317ba0ad85d
Oleksandr Yanov <oleksandr.yanov@gmail.com>
2023-01-17 08:14:59 +0200

    added few more lines of code from course, for postprocessing dataset

---
+++ fine_tuning_pretrained.py
@@ -0,0 +1,31 @@
+import torch
+print("Torch cuda available: ",torch.cuda.is_available())
+import tensorflow as tf
+print("tensorflow GPU list: ", tf.config.list_physical_devices('GPU'))
+# exit()
+import os
+#  for laptop
+os.environ['HF_HOME'] = 'd:/Large data/qa data/hf_home/'
+os.environ['TRANSFORMERS_CACHE'] = 'd:/Large data/qa data/transformers/cache/'
+
+
+from datasets import load_dataset
+from transformers import AutoTokenizer, DataCollatorWithPadding
+
+raw_datasets = load_dataset("glue", "mrpc")
+checkpoint = "bert-base-uncased"
+tokenizer = AutoTokenizer.from_pretrained(checkpoint)
+
+
+def tokenize_function(example):
+    return tokenizer(example["sentence1"], example["sentence2"], truncation=True)
+
+
+tokenized_datasets = raw_datasets.map(tokenize_function, batched=True)
+data_collator = DataCollatorWithPadding(tokenizer=tokenizer)
+
+tokenized_datasets = tokenized_datasets.remove_columns(["sentence1", "sentence2", "idx"])
+tokenized_datasets = tokenized_datasets.rename_column("label", "labels")
+tokenized_datasets.set_format("torch")
+print(tokenized_datasets["train"].column_names)
+
