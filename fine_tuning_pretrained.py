881136a50bfdf205b696bd95af9b0cffd757d787
Oleksandr Yanov <oleksandr.yanov@gmail.com>
2023-01-18 09:03:17 +0200

    added customer training loop with accelerator,  trainigns works but now evaluation does not

--- fine_tuning_pretrained.py
+++ fine_tuning_pretrained.py
@@ -29,3 +29,116 @@ tokenized_datasets = tokenized_datasets.rename_column("label", "labels")
 tokenized_datasets.set_format("torch")
 print(tokenized_datasets["train"].column_names)
 
+from torch.utils.data import DataLoader
+
+train_dataloader = DataLoader(
+    tokenized_datasets["train"], shuffle=True, batch_size=8, collate_fn=data_collator
+)
+eval_dataloader = DataLoader(
+    tokenized_datasets["validation"], batch_size=8, collate_fn=data_collator
+)
+
+for batch in train_dataloader:
+    break
+print({k: v.shape for k, v in batch.items()})
+
+# common, without accelerator
+# from transformers import AutoModelForSequenceClassification
+# model = AutoModelForSequenceClassification.from_pretrained(checkpoint, num_labels=2)
+#
+# outputs = model(**batch)
+# print(outputs.loss, outputs.logits.shape)
+#
+# from transformers import AdamW
+# optimizer = AdamW(model.parameters(), lr=5e-5)
+#
+# from transformers import get_scheduler
+#
+# num_epochs = 3
+# num_training_steps = num_epochs * len(train_dataloader)
+# lr_scheduler = get_scheduler(
+#     "linear",
+#     optimizer=optimizer,
+#     num_warmup_steps=0,
+#     num_training_steps=num_training_steps,
+# )
+# print(num_training_steps)
+#
+# # ensure we use gpu
+# import torch
+# device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+# model.to(device)
+# print(device)
+#
+# # adding progress bar
+# from tqdm.auto import tqdm
+#
+# progress_bar = tqdm(range(num_training_steps))
+#
+# model.train()
+# for epoch in range(num_epochs):
+#     for batch in train_dataloader:
+#         batch = {k: v.to(device) for k, v in batch.items()}
+#         outputs = model(**batch)
+#         loss = outputs.loss
+#         loss.backward()
+#
+#         optimizer.step()
+#         lr_scheduler.step()
+#         optimizer.zero_grad()
+#         progress_bar.update(1)
+
+# with accelerator:
+from accelerate import Accelerator
+from transformers import AdamW, AutoModelForSequenceClassification, get_scheduler
+
+accelerator = Accelerator()
+
+model = AutoModelForSequenceClassification.from_pretrained(checkpoint, num_labels=2)
+optimizer = AdamW(model.parameters(), lr=3e-5)
+
+train_dl, eval_dl, model, optimizer = accelerator.prepare(
+    train_dataloader, eval_dataloader, model, optimizer
+)
+
+num_epochs = 3
+num_training_steps = num_epochs * len(train_dl)
+lr_scheduler = get_scheduler(
+    "linear",
+    optimizer=optimizer,
+    num_warmup_steps=0,
+    num_training_steps=num_training_steps,
+)
+
+# adding progress bar
+from tqdm.auto import tqdm
+progress_bar = tqdm(range(num_training_steps))
+
+model.train()
+for epoch in range(num_epochs):
+    for batch in train_dl:
+        outputs = model(**batch)
+        loss = outputs.loss
+        accelerator.backward(loss)
+
+        optimizer.step()
+        lr_scheduler.step()
+        optimizer.zero_grad()
+        progress_bar.update(1)
+
+# evaluation of model accuracy
+import evaluate
+
+metric = evaluate.load("glue", "mrpc")
+model.eval()
+device=accelerator.device
+for batch in eval_dataloader:
+    batch = {k: v.to(device) for k, v in batch.items()}
+    with torch.no_grad():
+        outputs = model(**batch)
+
+    logits = outputs.logits
+    predictions = torch.argmax(logits, dim=-1)
+    metric.add_batch(predictions=predictions, references=batch["labels"])
+
+print(metric.compute())
