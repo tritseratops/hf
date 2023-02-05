26fe4c1c2cc6594cf4e81754b502518824192dee
Oleksandr Yanov <oleksandr.yanov@gmail.com>
2023-02-05 07:54:17 +0200

    added save a load operations

--- l5_datasets_pandas.py
+++ l5_datasets_pandas.py
@@ -36,7 +36,32 @@ print(drug_dataset)
 # creating validation set
 drug_dataset_clean = drug_dataset["train"].train_test_split(train_size=0.8, seed=42)
 # rename efault test split into validation
-drug_dataset_clean["validation"] = drug_dataset_clean.pop("test")
-# add the "test" set to our DatasetDict
-drug_dataset_clean["test"]=drug_dataset["test"]
 print(drug_dataset_clean)
+drug_dataset_clean["validation"] = drug_dataset_clean.pop("test")
+print(drug_dataset_clean)
+# add the "test" set to our DatasetDict
+# drug_dataset_clean["test"]=drug_dataset["test"] # commented because only train file loaded
+drug_dataset_train_test = drug_dataset_clean["train"].train_test_split(train_size=0.6, seed=42)
+drug_dataset_clean["train"]=drug_dataset_train_test.pop("train")
+drug_dataset_clean["test"]=drug_dataset_train_test.pop("test")
+print(drug_dataset_clean)
+
+# commented to speed up
+# save_path = ds_file_path+"drug-ds-clean"
+# drug_dataset_clean.save_to_disk(save_path)
+#
+# from datasets import load_from_disk
+# loaded_ds = load_from_disk(save_path)
+# print(loaded_ds)
+
+save_path = ds_file_path+"json//"
+for split, dataset in drug_dataset_clean.items():
+    dataset.to_json(f"{save_path}drug-reviews-{split}.jsonl")
+
+data_files = {
+    "train": save_path+"drug-reviews-train.jsonl",
+    "validation": save_path+"drug-reviews-validation.jsonl",
+    "test": save_path+"drug-reviews-test.jsonl",
+}
+loaded_from_json = load_dataset("json", data_files=data_files)
+print(loaded_from_json)
