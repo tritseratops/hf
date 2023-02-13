71fe35e5d2a6b841284b878cac82ca5802f608ee
Oleksandr Yanov <oleksandr.yanov@gmail.com>
2023-02-13 08:44:13 +0200

    works after  pip install jupyter notebook

--- l5_creating_own_dataset_getting_data.py
+++ l5_creating_own_dataset_getting_data.py
@@ -17,3 +17,44 @@ cfg.read("config.ini")
 GITHUB_TOKEN = cfg['GITHUB']['GITHUB_TOKEN']
 print(GITHUB_TOKEN)
 headers = {"Authorization":f"token {GITHUB_TOKEN}"}
+import time
+import math
+from pathlib import Path
+import pandas as pd
+from tqdm.notebook import tqdm
+
+def fetch_issues(
+        owner="huggingface",
+        repo="datasets",
+        num_issues=10_000,
+        rate_limit=5_000,
+        issues_path=Path("."),
+):
+    if not issues_path.is_dir():
+        issues_path.mkdir(exist_ok=True)
+
+    batch = []
+    all_issues = []
+    per_page = 100 # number of issues to return per page
+    num_pages = math.ceil(num_issues / per_page)
+    base_url = "https://api.github.com/repos"
+
+    for page in tqdm(range(num_pages), position=0, desc="i", leave=False, ncols=80): #  colour='green',
+        print("test")
+        # query with state=all to get both open and closed issues
+        query = f"issues?page={page}&per_page={per_page}&state=all"
+        issues = requests.get(f"{base_url}/{owner}/{repo}/{query}", headers=headers)
+        batch.extend(issues.json())
+
+        if len(batch) > rate_limit and len(all_issues) < num_issues:
+            all_issues.extend(batch)
+            batch = [] # clear batch for next iteration
+            print(f"Reached Github rate limit. Sleeping for 1 hour...")
+            # time.sleep(60 * 60 +1)
+
+    all_issues.extend(batch)
+    df = pd.DataFrame.from_records(all_issues)
+    df.to_json(f"{issues_path}/{repo}-issues.jsonl", orient="records", lines=True)
+    print(f"Downloaded all the issues for {repo}! Dataset stored at {issues_path}/{repo}-issues.jsonl")
+
+fetch_issues()
