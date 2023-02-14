9a86ab01d8b6e361c4a7c4cc4774145a6d63cf33
Oleksandr Yanov <oleksandr.yanov@gmail.com>
2023-02-14 07:45:22 +0200

    works without otken but does not work with token, something in token is wrong

--- l5_creating_own_dataset_getting_data.py
+++ l5_creating_own_dataset_getting_data.py
@@ -4,19 +4,31 @@ os.environ['HF_HOME'] = 'd:/Large data/qa data/hf_home/'
 os.environ['TRANSFORMERS_CACHE'] = 'd:/Large data/qa data/transformers/cache/'
 
 import requests
-url = "https://api.github.com/repos/huggingface/datasets/issues?page=1&per_page=1"
-response = requests.get(url)
-print(response)
-print(response.status_code)
-print(response.json())
-
+# url = "https://api.github.com/repos/huggingface/datasets/issues?page=1&per_page=1"
+page = 1
+per_page = 1
+base_url = "https://api.github.com/repos"
+owner="huggingface"
+repo="datasets"
+query = f"issues?page={page}&per_page={per_page}&state=all"
 from configparser import ConfigParser
 cfg=ConfigParser()
 cfg.read("config.ini")
-
 GITHUB_TOKEN = cfg['GITHUB']['GITHUB_TOKEN']
 print(GITHUB_TOKEN)
 headers = {"Authorization":f"token {GITHUB_TOKEN}"}
+issues = requests.get(f"{base_url}/{owner}/{repo}/{query}", headers=headers)
+print(issues)
+print(issues.status_code)
+print(issues.json())
+
+# response = requests.get(url)
+# print(response)
+# print(response.status_code)
+# print(response.json())
+exit()
+
+
 import time
 import math
 from pathlib import Path
@@ -40,17 +52,18 @@ def fetch_issues(
     base_url = "https://api.github.com/repos"
 
     for page in tqdm(range(num_pages), position=0, desc="i", leave=False, ncols=80): #  colour='green',
-        print("test")
+        print("Page: ", page)
         # query with state=all to get both open and closed issues
         query = f"issues?page={page}&per_page={per_page}&state=all"
         issues = requests.get(f"{base_url}/{owner}/{repo}/{query}", headers=headers)
+        print(issues)
         batch.extend(issues.json())
 
         if len(batch) > rate_limit and len(all_issues) < num_issues:
             all_issues.extend(batch)
             batch = [] # clear batch for next iteration
             print(f"Reached Github rate limit. Sleeping for 1 hour...")
-            # time.sleep(60 * 60 +1)
+            time.sleep(60 * 60 +1)
 
     all_issues.extend(batch)
     df = pd.DataFrame.from_records(all_issues)
@@ -58,3 +71,7 @@ def fetch_issues(
     print(f"Downloaded all the issues for {repo}! Dataset stored at {issues_path}/{repo}-issues.jsonl")
 
 fetch_issues()
+from datasets import load_dataset
+# data_files = "./datasets-issues.jsonl"
+# issues_dataset = load_dataset("json", data_files, split="train")
+# print(issues_dataset)
