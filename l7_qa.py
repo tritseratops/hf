44385bcf0996d570d5361e998b0f6a6ad9dbb39a
Oleksandr Yanov <oleksandr.yanov@gmail.com>
2023-03-22 08:35:50 +0200

    finished preprocessing data for qa

--- l7_qa.py
+++ l7_qa.py
@@ -46,3 +46,139 @@ inputs = tokenizer(
 print(inputs.keys())
 for ids in inputs["input_ids"]:
     print(tokenizer.decode(ids))
+
+print("overflow_to_sample_mapping:")
+print(inputs["overflow_to_sample_mapping"])
+
+inputs = tokenizer(
+    raw_dataset["train"][2:6]["question"],
+    raw_dataset["train"][2:6]["context"],
+    max_length=100,
+    truncation="only_second",
+    stride=50,
+    return_overflowing_tokens=True,
+    return_offsets_mapping=True,
+)
+
+print(f"The 4 examples gave {len(inputs['input_ids'])} features")
+print(f"Here is where examples come from {inputs['overflow_to_sample_mapping']} features")
+
+answers = raw_dataset["train"][2:6]["answers"]
+start_positions = []
+end_positions = []
+
+for i, offset in enumerate(inputs["offset_mapping"]):
+    sample_idx = inputs["overflow_to_sample_mapping"][i]
+    answer = answers[sample_idx]
+    start_char = answer["answer_start"][0]
+    end_char = answer["answer_start"][0]+ len(answer["text"][0])
+    sequence_ids = inputs.sequence_ids(i)
+
+    # Find the start and end of the context
+    idx = 0
+    while sequence_ids[idx] != 1:
+        idx +=1
+    context_start = idx
+    while sequence_ids[idx] == 1:
+        idx += 1
+    context_end = idx - 1
+
+    # if the answer is not fully inside the context, label is (0,0)
+    if offset[context_start][0] > start_char or offset[context_end][1] < end_char:
+        start_positions.append(0)
+        end_positions.append(0)
+    else:
+        # otherwise its the start and end token positions
+        idx = context_start
+        while idx<=context_end and offset[idx][0] <= start_char:
+            idx += 1
+        start_positions.append(idx-1)
+        while idx >= context_start and offset[idx][0] <= start_char:
+            idx -= 1
+        end_positions.append(idx+1)
+print(start_positions, end_positions)
+
+# lets compare the theoretical answer with the decoded span of tokens
+idx = 0
+sample_idx = inputs["overflow_to_sample_mapping"][idx]
+answer = answers[sample_idx]["text"][0]
+
+start  = start_positions[idx]
+end = end_positions[idx]
+labeled_answer = tokenizer.decode(inputs["input_ids"][idx][start : end + 1])
+
+print(f"Theoretical answer: {answer}, labels give: {labeled_answer}")
+
+# lets check answer that is not in context
+idx = 4
+sample_idx = inputs["overflow_to_sample_mapping"][idx]
+answer = answers[sample_idx]["text"][0]
+
+decoded_example = tokenizer.decode(inputs["input_ids"][idx])
+print(f"Theoretical answer: {answer}, decoded example: {decoded_example}")
+
+# grouping preporcessing to a function to use in map method
+max_length = 384
+stride = 128
+
+def preprocess_training_example(examples):
+    questions  = [q.strip() for q in examples["question"]]
+    inputs = tokenizer(
+        questions,
+        examples["context"],
+        max_length=max_length,
+        truncation = "only_second",
+        stride=stride,
+        return_overflowing_tokens=True,
+        return_offsets_mapping=True,
+        padding="max_length",
+    )
+
+    offset_mapping = inputs.pop("offset_mapping")
+    sample_map = inputs.pop("overflow_to_sample_mapping")
+    answers = examples["answers"]
+    start_positions = []
+    end_positions = []
+
+    for i, offset in enumerate(offset_mapping):
+        sample_idx = sample_map[i]
+        answer = answers[sample_idx]
+        start_char = answer["answer_start"][0]
+        end_char = answer["answer_start"][0] + len(answer["text"][0])
+        sequence_ids = inputs.sequence_ids(i)
+
+        # Find the start and end oif the context
+        idx = 0
+        while sequence_ids[idx] != 1:
+            idx += 1
+        context_start = idx
+        while sequence_ids[idx] == 1:
+            idx += 1
+        context_end = idx - 1
+
+        # if the answer is not fully inside the context label is (0,0)
+        if offset[context_start][0] > start_char or offset[context_end][1] < end_char:
+            start_positions.append(0)
+            end_positions.append(0)
+        else:
+            # Otherwise it's the start and end token positions
+            idx = context_start
+            while idx <= context_end and offset[idx][0] <= start_char:
+                idx += 1
+            start_positions.append(idx - 1)
+
+            idx = context_end
+            while idx >= context_end and offset[idx][0] >= end_char:
+                idx -= 1
+            end_positions.append(idx + 1)
+    inputs["start_positions"] = start_positions
+    inputs["end_positions"] = end_positions
+    return inputs
+
+train_dataset = raw_dataset["train"].map(
+    preprocess_training_example,
+    batched=True,
+    remove_columns=raw_dataset["train"].column_names,
+)
+
+print(len(raw_dataset["train"]), len(train_dataset))
